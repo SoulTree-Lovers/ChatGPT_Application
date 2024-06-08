0a9b22ef0981791313e5d930c8046af298d2e94b
kangseungmin <rkdtmdals999@naver.com>
2024-06-08 21:07:31 +0900

    feat: SummaryRepository 생성

--- src/main/java/org/example/link/SummaryRepository.java
+++ src/main/java/org/example/link/SummaryRepository.java
@@ -1,7 +1,10 @@
 package org.example.link;
 
-//import org.example.link.entity.SummaryEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface SummaryRepository extends JpaRepository<SummaryEntity, Long> {
-//}
+import org.example.link.entity.SummaryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface SummaryRepository extends JpaRepository<SummaryEntity, Long> {
+
+
+}
