d9bfecc19f550455fd3b2ed9d4d19b97c7567c45
kangseungmin <rkdtmdals999@naver.com>
2024-06-09 16:09:36 +0900

    feat: 서비스 로직 구현

--- src/main/java/org/example/link/SummaryService.java
+++ src/main/java/org/example/link/SummaryService.java
@@ -1,25 +1,28 @@
 package org.example.link;
 
-//import org.example.link.entity.SummaryEntity;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.example.link.entity.SummaryEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Service
-//public class SummaryService {
-//
-//    @Autowired
-//    private SummaryRepository summaryRepository;
-//
-//    public SummaryEntity saveSummary(String url, String summary) {
-//        SummaryEntity newSummary = new SummaryEntity();
-//        newSummary.setUrl(url);
-//        newSummary.setSummary(summary);
-//        return summaryRepository.save(newSummary);
-//    }
-//
-//    public List<SummaryEntity> getAllSummaries() {
-//        return summaryRepository.findAll();
-//    }
-//}
+@Service
+@RequiredArgsConstructor
+public class SummaryService {
+
+    private final SummaryRepository summaryRepository;
+
+    public SummaryEntity saveSummary(String url, String summary) {
+
+        return summaryRepository.save(SummaryEntity.builder()
+            .url(url)
+            .summary(summary)
+            .build());
+
+    }
+
+    public List<SummaryEntity> getAllSummaries() {
+        return summaryRepository.findAll();
+    }
+}
