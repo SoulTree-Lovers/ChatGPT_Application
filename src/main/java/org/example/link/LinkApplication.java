13fa328f6fb11d68da59cbfb6b3e4d63cc89e00f
kangseungmin <rkdtmdals999@naver.com>
2024-06-09 16:09:28 +0900

    feat: EntityScan 어노테이션 추가

--- src/main/java/org/example/link/LinkApplication.java
+++ src/main/java/org/example/link/LinkApplication.java
@@ -2,8 +2,10 @@ package org.example.link;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan
 public class LinkApplication {
 
     public static void main(String[] args) {
