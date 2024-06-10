22473f968b7c06563cbc40ae4afb75bdd096fecd
kangseungmin <rkdtmdals999@naver.com>
2024-06-10 22:03:00 +0900

    feat: EnableJpaAuditing 어노테이션 추가
    createdAt, updatedAt 자동 생성 어노테이션 사용 위해

--- src/main/java/org/example/link/LinkApplication.java
+++ src/main/java/org/example/link/LinkApplication.java
@@ -3,9 +3,12 @@ package org.example.link;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EntityScan
+@EnableJpaAuditing
 public class LinkApplication {
 
     public static void main(String[] args) {
