6190ef1d5f1147379aa395297fb422bdd4c078c1
kangseungmin <rkdtmdals999@naver.com>
2024-06-10 22:04:59 +0900

    feat: 빌더 패턴 추가

--- src/main/java/org/example/link/entity/ChatMessage.java
+++ src/main/java/org/example/link/entity/ChatMessage.java
@@ -1,12 +1,14 @@
 package org.example.link.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ChatMessage {
     private String role;
     private String content;
