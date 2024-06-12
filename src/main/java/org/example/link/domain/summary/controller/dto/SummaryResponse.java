b95e5017004f443246b476890b863019097b3569
kangseungmin <rkdtmdals999@naver.com>
2024-06-12 12:12:36 +0900

    feat: SummaryResponse DTO 생성

---
+++ src/main/java/org/example/link/domain/summary/controller/dto/SummaryResponse.java
@@ -0,0 +1,22 @@
+package org.example.link.domain.summary.controller.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class SummaryResponse {
+
+    private String url;
+
+    private String summaryContent;
+
+}
